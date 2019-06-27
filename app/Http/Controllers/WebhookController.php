577bc9b8ce9c4f1dcce90c87db7861b183ef00ea
r567tw <r567tw@gmail.com>
2019-06-28 01:10:33 +0800

    remove variable to env

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -9,12 +9,15 @@ use LINE\LINEBot\Constant\HTTPHeader;
 class WebhookController extends Controller
 {
 
-    private $token = 'oquKUUZO1oX2p8LMV4V0fI1i8KmkYnhxf+jW6UxkNSk11qBcXW2kMS7X9e5fBl3GRjYZBpl3Q4qVGGP04cIuXnQzzKNO3+W+xo3EqGmqbbl/eE31uftzg6c2paeTekA4KXtELUEanhpIOze3RF7q3wdB04t89/1O/w1cDnyilFU=';
-    private $secret = '8acbb1b97ad224edbb606c7af87fa7c0';
+    private $token = '';
+    private $secret = '';
 
 
     public function __construct()
     {
+        $this->token = env('LINEBOT_TOKEN');
+        $this->secret = env('LINEBOT_SECRET');
+
         $this->httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($this->token);
 
         $this->bot = new \LINE\LINEBot($this->httpClient, ['channelSecret' => $this->secret]);
